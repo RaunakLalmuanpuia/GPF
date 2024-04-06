1198ee5f457c50cc40deeb738c966a6ac6c6f005
Raunak <Raunak.msegs@gmail.com>
2024-04-06 14:41:33 +0530

    quasar installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,16 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import app from './components/app.vue';
-
+import { Quasar } from 'quasar'
 import router from './router/index.js';
 
-createApp(app).use(router).mount('#app')
+// Import icon libraries
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
+
+createApp(app).use(router).use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  }).mount('#app')
--- resources/js/components/GPF/print.vue
+++ resources/js/components/GPF/print.vue
@@ -1,6 +1,21 @@
 <template>
 <p>print Gpf</p>
 {{ form }}
+
+<div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+  </div>
 </template>
 
 <script setup>
@@ -10,6 +25,8 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 let form = ref({id:''})
+
+let slide = ref(1);
 
 const props = defineProps({
     id:{
@@ -26,5 +43,7 @@ const getGpf = async () => {
     let response = await axios.get(`/api/show_gpf/${props.id}`)
     form.value = response.data.entry_info
 }
+
+
 
 </script>
