11383c8d781d7bf8fda5f028d9e804085a54e8db
Raunak <Raunak.msegs@gmail.com>
2024-04-17 22:00:53 +0530

    vite build

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
+    mode: 'production',
     plugins: [
         laravel({
-            input: "resources/js/app.js",
+            input: ["resources/js/app.js", "resources/css/app.css"],
             refresh: true,
             template: { transformAssetUrls },
         }),
