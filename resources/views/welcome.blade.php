c382a12990bd1da5747b34b10358a46cfca047c1
Raunak <Raunak.msegs@gmail.com>
2024-04-20 10:16:51 +0530

    build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link rel="preconnect" href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         @vite('resources/css/app.css') 
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
        
     </head>
     <body>
